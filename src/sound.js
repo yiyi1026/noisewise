96ca7af2ca3e41f08cdf22826397f432bff6ec0a
yiyi1026 <vivian.li.uibe@gmail.com>
2018-02-28 20:36:57 -0800

    refactor sound component

--- src/sound.js
+++ src/sound.js
@@ -6,14 +6,13 @@ class Sound extends Component {
   constructor(props){
     super(props);
     let audio= new Audio(`/../sounds/${this.props.name}.mp3`);
-    // let audio= new Audio("/../sounds/dryer.mp3")
+    audio.volume = 0.2;
 
-    audio.volume = 0.2
     this.state = {
       audio,
       toggle: false,
       volume: 0.2,
-      visibility: 'hidden'
+      visibility: "hidden"
     };
   }
 
@@ -29,30 +28,25 @@ class Sound extends Component {
   }
 
   visibility(toggle){
-    if(toggle){
-      // console.log('v');
-      this.setState({visibility: 'visible'});
+    let visibility;
+    if (toggle){
+      visibility = "visible";
     }else{
-      // console.log('h');
-      this.setState({visibility: 'hidden'});;
+      visibility = "hidden";
     }
+    this.setState({visibility});
+
   };
 
   play(){
     let audio = this.state.audio;
-    // console.log(this.props.muted);
-    // if (this.props.muted === true){
-    //   audio.muted = true;
-    // }else{
-    //   audio.muted = false;
-    // }
-    if(this.state.toggle){
+    let toggle = this.state.toggle;
+    if(toggle){
       audio.pause();
-      this.setState({toggle: false}, () => this.visibility(this.state.toggle));
     } else {
       audio.play();
-      this.setState({toggle: true}, () => this.visibility(this.state.toggle));
     }
+    this.setState({toggle: !toggle}, () => this.visibility(this.state.toggle));
   }
 
   volumeAdjust(e) {
@@ -66,11 +60,12 @@ class Sound extends Component {
   }
 
   render() {
+    let name = this.props.name;
     let opacity = "1";
-    if(this.state.visibility === 'hidden'){
+
+    if(this.state.visibility === "hidden"){
       opacity = "0.4";
     }
-    let name = this.props.name;
 
     return (
       <div className="sound col-lg-2 col-md-2 col-sm-3 col-xs-3">
@@ -80,7 +75,7 @@ class Sound extends Component {
           </a>
           <Range min={0} max={1} step={0.1} className={`slider ${this.state.visibility}`} value={this.state.volume} onChange={(e) => this.volumeAdjust(e)}/>
         </div> 
-        </div>
+      </div>
     );
   }
 }
