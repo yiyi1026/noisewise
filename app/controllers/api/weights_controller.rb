ffa57735704f61c84155b99c5aa4201d3c802e60
yiyi1026 <vivian.li.uibe@gmail.com>
2018-03-02 13:36:25 -0800

    add weights controller routes

--- app/controllers/api/weights_controller.rb
+++ app/controllers/api/weights_controller.rb
@@ -1,2 +1,61 @@
 class Api::WeightsController < ApplicationController
+
+  def create
+    @weight = current_user.weights.new(weight_params)
+
+    if @weight.save
+      render json: @weight
+    else
+      render json: @weight.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @weight = Weight.find(params[:id])
+    render :show
+  end
+
+  def update
+    @weight = Weight.find(params[:id])
+    if @weight
+      if @weight.update_attributes(weight_params)
+        render :show
+      end
+    else
+      render json: @weight.errors.full_messages, status: 422
+    end
+  end
+
+  def search
+    if params[:query].present?
+      @weights = Weight.where("title LIKE ?", "%#{params[:query]}%").limit(10)
+      render :search
+    else
+      @weights = Weight.all
+      render :search
+    end
+  end
+
+
+  def index
+    if params[:query].present?
+      @weights = Weight.where("title LIKE ?", "%#{params[:query]}%").limit(10)
+      render :index
+    else
+      @weights = Weight.all
+      render :index
+    end
+  end
+
+  def destroy
+    @weight = current_user.weights.find(params[:id])
+    @weight.destroy
+    render json: @weight
+  end
+
+  private
+
+  def weight_params
+    params.require(:weight).permit(:username, :user_id, :weight, :date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
+# == Route Map
+#
+#           Prefix Verb   URI Pattern                 Controller#Action
+#        api_users POST   /api/users(.:format)        api/users#create {:format=>:json}
+#         api_user GET    /api/users/:id(.:format)    api/users#show {:format=>:json}
+#  new_api_weights GET    /api/weights/new(.:format)  api/weights#new {:format=>:json}
+# edit_api_weights GET    /api/weights/edit(.:format) api/weights#edit {:format=>:json}
+#      api_weights GET    /api/weights(.:format)      api/weights#show {:format=>:json}
+#                  PATCH  /api/weights(.:format)      api/weights#update {:format=>:json}
+#                  PUT    /api/weights(.:format)      api/weights#update {:format=>:json}
+#                  DELETE /api/weights(.:format)      api/weights#destroy {:format=>:json}
+#                  POST   /api/weights(.:format)      api/weights#create {:format=>:json}
+#             root GET    /                           static_pages#root
+# 
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+    resource :weights
+  end
+
   root "static_pages#root"
 end
