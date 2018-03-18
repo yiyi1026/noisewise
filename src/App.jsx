36d23c0afd142921b5359b65fdd72561638298c3
yiyi1026 <vivian.li.uibe@gmail.com>
2018-03-17 19:16:23 -0700

    add navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 import NoiseMain from './noise/noisemain.jsx';
+import { Link } from 'react-router-dom';
 // import Test from './noise/test';
 import WeightWatcherContainer from './components/weights/weightwatcher_container.jsx';
 
@@ -9,9 +10,20 @@ import { Switch, Route , HashRouter} from 'react-router-dom';
 const App = () => {
   return (
     <div>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">NoiseWise</Link>
+          </div>
+          <ul className="nav navbar-nav">
+            <li><Link to="/noise">NoiseWise</Link></li>
+            <li><Link to="/weights">Weight Watcher</Link></li>
+          </ul>
+        </div>
+      </nav>
       <Switch>
-        <Route path="/weights" component={WeightWatcherContainer}/>
-        <Route path="/noise" component={NoiseMain}/>
+        <Route exact path="/weights" component={WeightWatcherContainer}/>
+        <Route exact path="/noise" component={NoiseMain}/>
       </Switch>
       </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import {HashRouter} from 'react-router-dom';
 import configureStore from './store/store';
