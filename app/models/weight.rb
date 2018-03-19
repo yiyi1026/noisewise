cda189fefaf468f062fc23549fca530f534b4771
yiyi1026 <vivian.li.uibe@gmail.com>
2018-03-19 00:32:48 -0700

    add validation for uniqueness of weight

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -13,6 +13,8 @@
 class Weight < ApplicationRecord
   validates :user_id, :user, :date, :weight_value, presence: true
 
+  # each user should have at most one weight for each day
+  validates_uniqueness_of :date, scope: %i[user_id]
   #associations
   belongs_to :user
 
